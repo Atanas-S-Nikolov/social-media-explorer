6478f6d84cd50dfca83480a3071cee88e3bc6a79
Atanas-S-Nikolov <atanas.stoyanov1199@gmail.com>
2023-09-15 00:32:44 +0300

    Fix footer year

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,11 +7,13 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
+const CURRENT_YEAR = new Date().getFullYear();
+
 export default function Footer() {
   return (
     <footer className="footer">
       <hr className='divider'/>
-      <p>© 2023 Social Media Explorer. All rights reserved.</p>
+      <p>© {CURRENT_YEAR} Social Media Explorer. All rights reserved.</p>
       <section className='contacts_section'>
         <span className='contacts_section_text'>Developer contacts:</span>
         <section className='links'>
